78dfc9c273f32ab695d2fcbd20500932f8cd599b
dilen shankar <elchurrocuandotebese@gmail.com>
2025-08-06 22:18:14 +0530

    Update analyze.py

--- monitor/analyze.py
+++ monitor/analyze.py
@@ -1,1 +1,11 @@
+from scapy.all import rdpcap
+
+def extract_latency(pcap_file):
+    packets = rdpcap(pcap_file)
+    times = [pkt.time for pkt in packets if pkt.haslayer('ICMP')]
+    if len(times) >= 2:
+        return times[1] - times[0]  
+#dilen- this gives a rough RTT estimate
+    return None
+
 
