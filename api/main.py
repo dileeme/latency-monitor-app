336408023ef503f24006e93921c48d15739a3d7b
dilen shankar <elchurrocuandotebese@gmail.com>
2025-08-06 22:19:50 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,1 +1,13 @@
+from flask import Flask, jsonify
+from latency_parser import extract_latency
+
+app = Flask(__name__)
+
+@app.route('/latency')
+def get_latency():
+    latency = extract_latency('latest.pcap')
+    return jsonify({"latency_ms": latency * 1000})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
